b5c7548df731f8e6d5b532f22a29c3b25200eb46
Gus-oss <gustavo223@live.com.mx>
2026-07-19 13:34:57 -0600

    Creo el codigo y subo los archivos del sonido a la carpeta de tarea 7

---
+++ Procesamiento-Clasificacion-Datos/test/tarea6/audio.py
@@ -0,0 +1,11 @@
+#audio.py
+#===========================================================================================
+#Librerias
+#===========================================================================================
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import IPython
+
+IPython.display.Audio('C:\Users\PC\Documents\DocumentosGustavo\Github\Maestria\Procesamiento-Clasificacion-Datos\Procesamiento-Clasificacion-Datos\data\raw\tarea7\bisicleta_alquiler.mp3')
